ddf1a7c3144e1c15cd2691a1343986bfde15b0b5
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2023-11-04 14:58:56 +0100

    favicon

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ use std::{error::Error, fs, process::Command};
 
 markup::define! {
     Button(href: &'static str, icon: &'static str, name: &'static str, alt: &'static str) {
-        a[href=href,target="_blank",class="btn"] {
+        a[
+            href=href,
+            target="_blank",
+            class="
+                flex flex-row gap-1 justify-center items-center p-2 bg-gradient-to-r from-cyan-300 to-blue-300
+                rounded-xl dark:from-cyan-500 dark:to-blue-500 hover:from-cyan-400 hover:to-blue-400 focus:ring
+                focus:outline-none active:from-cyan-500 active:to-blue-500 print:hidden dark:hover:from-cyan-600
+                dark:hover:to-blue-600 dark:active:from-cyan-700 dark:active:to-blue-700
+            ",
+        ] {
             span[class=format!("w-6 h-6 stroke-2 {icon}")]{}
             @name
         }
@@ -12,7 +21,14 @@ markup::define! {
 
 markup::define! {
     Skill(href: &'static str, name: &'static str, icon: Option<&'static str>, color: Option<&'static str>) {
-        a[href=href,target="_blank",class="skill"] {
+        a[
+            href=href,
+            target="_blank",
+            class="
+                flex flex-row items-center rounded-sm focus:ring focus:ring-blue-500 focus:outline-none
+                focus:ring-offset-3 focus:dark:ring-offset-stone-900
+            ",
+        ] {
             @if let Some((icon, color)) = icon.zip(*color) {
                 span[class=format!("{icon} mr-1 h-8 w-8 {color} print:hidden")]{}
             }
@@ -72,11 +88,10 @@ markup::define! {
                 meta[charset="utf-8"];
                 meta[name="viewport",content="width=device-width,initial-scale=1"];
                 title { "Dan Bluhm Hansen - CV" }
-                link[
-                  rel="icon",
-                  type="image/svg+xml",
-                  href="data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8' standalone='no'%3F%3E%3Csvg width='385pt' height='385pt' viewBox='0 0 385 385' version='1.1' id='svg30' xmlns:xlink='https://w3.org/1999/xlink' xmlns='https://w3.org/2000/svg' xmlns:svg='https://w3.org/2000/svg'%3E%3Cdefs id='defs16'%3E%3Cg id='g11'%3E%3Cg id='glyph-0-0'%3E%3Cpath d='M 7.921875 0 L 7.921875 -184.921875 L 145.296875 -184.921875 L 145.296875 0 Z M 76.609375 -109.109375 L 114.921875 -163.78125 L 38.3125 -163.78125 Z M 31.703125 -44.90625 L 64.984375 -92.453125 L 31.703125 -140.28125 Z M 121.515625 -44.90625 L 121.515625 -140.28125 L 88.234375 -92.453125 Z M 38.3125 -21.140625 L 114.921875 -21.140625 L 76.609375 -76.078125 Z M 38.3125 -21.140625 ' id='path2' /%3E%3C/g%3E%3Cg id='glyph-0-1'%3E%3Cpath d='M 39.625 -21.140625 L 128.125 -21.140625 L 128.125 -163.78125 L 39.625 -163.78125 Z M 15.84375 0 L 15.84375 -184.921875 L 130.765625 -184.921875 L 151.90625 -163.78125 L 151.90625 -21.140625 L 130.765625 0 Z M 15.84375 0 ' id='path5' /%3E%3C/g%3E%3Cg id='glyph-0-2'%3E%3Cpath d='M 15.84375 0 L 15.84375 -184.921875 L 39.625 -184.921875 L 39.625 -105.671875 L 137.375 -105.671875 L 137.375 -184.921875 L 161.140625 -184.921875 L 161.140625 0 L 137.375 0 L 137.375 -84.53125 L 39.625 -84.53125 L 39.625 0 Z M 15.84375 0 ' id='path8' /%3E%3C/g%3E%3C/g%3E%3CclipPath id='clip-0'%3E%3Cpath clip-rule='nonzero' d='M 0 0 L 385 0 L 385 385 L 0 385 Z M 0 0 ' id='path13' /%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23clip-0)' id='g20'%3E%3Cpath fill-rule='nonzero' fill='%231e1e2e' fill-opacity='1' d='m 0.375,0.375 h 384.25 v 384.25 H 0.375 Z m 0,0' id='path18' /%3E%3C/g%3E%3Cg fill='%23f5e0dc' fill-opacity='1' id='g24'%3E%3Cuse xlink:href='%23glyph-0-1' x='19.747196' y='284.43466' id='use22' /%3E%3C/g%3E%3Cg fill='%23f5e0dc' fill-opacity='1' id='g28'%3E%3Cuse xlink:href='%23glyph-0-2' x='187.49611' y='284.43466' id='use26' /%3E%3C/g%3E%3C/svg%3E%0A",
-                ];
+                link[rel="apple-touch-icon",sizes="180x180",href="apple-touch-icon.png"];
+                link[rel="icon",type="image/png",sizes="32x32",href="favicon-32x32.png"];
+                link[rel="icon",type="image/png",sizes="16x16",href="favicon-16x16.png"];
+                link[rel="manifest",href="site.webmanifest"];
                 link[
                     rel="stylesheet",
                     type="text/css",
@@ -84,7 +99,10 @@ markup::define! {
                 ];
                 link[rel="stylesheet",type="text/css",href="site.css"];
             }
-            body[class="container p-4 min-w-full min-h-screen sm:p-8 dark:bg-gradient-to-br dark:from-slate-900 dark:to-stone-900"] {
+            body[class="
+                container p-4 min-w-full min-h-screen sm:p-8 dark:bg-gradient-to-br dark:from-slate-900
+                dark:to-stone-900
+            "] {
                 div[class="mx-auto max-w-screen-lg"] {
                     header[class="flex flex-row justify-between items-center"] {
                         h1[class="text-5xl print:text-3xl"] { "Dan Bluhm Hansen" }
@@ -447,7 +465,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         hobbies_h: "Hobbies",
         hobbies: hobbies_en,
         profile_h: "Profile",
-        profile: "I am a passionate software developer who strives to build secure, stable, and fast software solutions. I spend much of my time researching the latest frameworks and tools so that I can keep up with the ever-changing software world. I love to learn and to share what I have learned with my colleagues, so we can all enjoy developing software together. What I like most about software development is building tools and foundations for others so they can focus on implementation and business logic. As a person, I am modest, calm, and always willing to help others.",
+        profile: "
+            I am a passionate software developer who strives to build secure, stable, and fast software solutions. I
+            spend much of my time researching the latest frameworks and tools so that I can keep up with the
+            ever-changing software world. I love to learn and to share what I have learned with my colleagues, so we can
+            all enjoy developing software together. What I like most about software development is building tools and
+            foundations for others so they can focus on implementation and business logic. As a person, I am modest,
+            calm, and always willing to help others.
+        ",
         exp_h: "Experience",
         edu: "Academy Profession (AP) degree in Computer Science - Zealand Institute of Business and Technology",
         experiences: experiences_en,
@@ -465,7 +490,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         hobbies_h: "Hobbyer",
         hobbies: hobbies_da,
         profile_h: "Profil",
-        profile: "Jeg er en passioneret softwareudvikler, der stræber efter at bygge sikre, stabile og hurtige softwareløsninger. Jeg bruger meget af min tid på at undersøge de nyeste frameworks og værktøjer, så jeg kan holde trit med den evigt udviklende softwareverden. Jeg elsker at lære og dele det jeg har lært med mine kolleger, så vi alle kan nyde at udvikle software sammen. Det jeg bedst kan lide ved softwareudvikling, er at bygge værktøjer og fundamenter for andre, så de kan fokusere på implementering og forretningslogik. Som person er jeg beskeden, rolig og altid villig til at hjælpe andre.",
+        profile: "
+            Jeg er en passioneret softwareudvikler, der stræber efter at bygge sikre, stabile og hurtige
+            softwareløsninger. Jeg bruger meget af min tid på at undersøge de nyeste frameworks og værktøjer, så jeg kan
+            holde trit med den evigt udviklende softwareverden. Jeg elsker at lære og dele det jeg har lært med mine
+            kolleger, så vi alle kan nyde at udvikle software sammen. Det jeg bedst kan lide ved softwareudvikling, er
+            at bygge værktøjer og fundamenter for andre, så de kan fokusere på implementering og forretningslogik. Som
+            person er jeg beskeden, rolig og altid villig til at hjælpe andre.
+        ",
         exp_h: "Erfaring",
         edu: " Datamatiker - Erhvervsakademi Sjælland ",
         experiences: experiences_da,
