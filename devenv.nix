e91ce6d5e59cfc6739431f3bc1f1fb61dfbc010f
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2024-01-20 16:16:35 +0000

    devenv, cargo-watch and miniserve

--- devenv.nix
+++ devenv.nix
@@ -4,35 +4,37 @@
   devcontainer.enable = true;
 
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  languages.rust = {
-    enable = true;
-    channel = "stable";
-  };
+  env.MINISERVE_PATH = "_site";
+  env.MINISERVE_INDEX = "index.html";
 
   # https://devenv.sh/packages/
   packages = [
     pkgs.git
     pkgs.bun
+    pkgs.cargo-watch
+    pkgs.miniserve
   ];
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
+  # scripts.hello.exec = "echo hello from $GREET";
 
-  enterShell = ''
-    hello
-    git --version
-  '';
+  # enterShell = ''
+  #   hello
+  #   git --version
+  # '';
 
   # https://devenv.sh/languages/
-  # languages.nix.enable = true;
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+  };
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
 
   # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
+  processes.watch.exec = "cargo watch --exec run";
+  processes.serve.exec = "miniserve";
 
   # See full reference at https://devenv.sh/reference/options/
 }
