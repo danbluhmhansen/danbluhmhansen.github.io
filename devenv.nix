f07c9ddc276ccb50de61e664cdfea737f76ca19b
Dan Bluhm Hansen <16237377+danbluhmhansen@users.noreply.github.com>
2024-01-26 21:19:37 +0100

    replaced miniserve with caddy

--- devenv.nix
+++ devenv.nix
@@ -1,18 +1,16 @@
 { pkgs, ... }:
 
 {
-  devcontainer.enable = true;
-
   # https://devenv.sh/basics/
-  env.MINISERVE_PATH = "_site";
-  env.MINISERVE_INDEX = "index.html";
+
+  # https://devenv.sh/integrations/codespaces-devcontainer/
+  devcontainer.enable = true;
 
   # https://devenv.sh/packages/
   packages = [
     pkgs.git
     pkgs.bun
     pkgs.cargo-watch
-    pkgs.miniserve
   ];
 
   # https://devenv.sh/scripts/
@@ -24,17 +22,24 @@
   # '';
 
   # https://devenv.sh/languages/
-  languages.rust = {
-    enable = true;
-    channel = "stable";
-  };
+  languages.rust.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
 
   # https://devenv.sh/processes/
   processes.watch.exec = "cargo watch --exec run";
-  processes.serve.exec = "miniserve";
+
+  # https://devenv.sh/services/
+  services.caddy = {
+    enable = true;
+    config = ''
+      localhost:8080
+
+      root * ./_site
+      file_server
+    '';
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
